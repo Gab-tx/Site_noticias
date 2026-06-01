2f474d21618cd808c93260776086146d078760d0
Gab-tx <gabrieltx040413@gmail.com>
2026-06-01 16:48:08 -0300

    Feat(Admin): Adicionado noticias já publicadas na interface do admin baseado em um arquivo json

--- admin/admin.js
+++ admin/admin.js
@@ -1,3 +1,4 @@
+
 const logado = localStorage.getItem("logado");
 
 if (logado !== "true") {
@@ -8,5 +9,47 @@ const logout = document.getElementById("logout")
 
 logout.addEventListener("click", () =>{
     localStorage.removeItem("logado");
-    window.location.href = "index.html";
+    window.location.href = "/index.html";
 });
+
+let dados = null;
+
+fetch('/mockup.json')
+    .then(res => res.json())
+    .then(json => {
+        dados = json;
+        renderNoticias();
+    });
+
+function renderNoticias() {
+    const container = document.getElementById("lista-noticias")
+
+    container.innerHTML = "";
+
+    dados.noticia.forEach(noticia => {
+        
+        const imagem = dados.imagem.find(img => img.idNoticia === noticia.idNoticia);
+        const card = document.createElement("article");
+        card.classList.add("noticia-card");
+
+        card.innerHTML = `
+        <img src="${imagem ? imagem.url : ''}"/>
+        <div aria-label="corpo do conteúdo" class="content">
+            <h3>${noticia.titulo}</h3>
+            <p>${noticia.descricao}</p>
+
+            <div class="actions">
+                <button class="edit">Editar</button>
+                <button class="delete" onclick="deletarNoticia(${noticia.idNoticia})">Excluir</button>
+            </div>
+        </div>
+        `;
+
+        container.appendChild(card)
+    });
+};
+
+function deletarNoticia(id) {
+    dados.noticia = dados.noticia.filter(n => n.idNoticia !== id);
+    renderNoticias();
+}
