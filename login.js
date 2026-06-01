15ba493014277753661fb83d8720af928fbc83d8
Gab-tx <gabrieltx040413@gmail.com>
2026-06-01 14:48:13 -0300

    Feat(Login): Adicionado lógica simples de login na página admin

---
+++ login.js
@@ -0,0 +1,19 @@
+const form = document.getElementById("login-form");
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    const email = document.getElementById("email").value;
+    const senha = document.getElementById("senha").value;
+    const mensagem = document.getElementById("mensagem");
+
+    const admsenha = "admin"
+    const admemail = "admin@gmail.com"
+
+    if (email === admemail && senha === admsenha) {
+        window.location.href = "admin.html";
+    } else {
+        mensagem.textContent = "Email ou senha inválidos.";
+    }
+
+});
