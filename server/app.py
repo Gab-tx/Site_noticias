4a00fab676cdbb74abee97307d2c639ef168a2c6
Gab-tx <gabrieltx040413@gmail.com>
2026-06-09 15:19:44 -0300

    Feat(app.py): Adicionado o CORS

--- server/app.py
+++ server/app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask_cors import CORS
 from routes.admin import admin_bp
 from routes.categoria import categoria_bp
 from routes.imagem import imagem_bp
@@ -6,10 +7,14 @@ from routes.noticia import noticia_bp
 
 app = Flask(__name__)
 
+CORS(app)
+
 app.register_blueprint(noticia_bp)
 app.register_blueprint(categoria_bp)
 app.register_blueprint(imagem_bp)
 app.register_blueprint(admin_bp)
+
+
 
 if __name__=="__main__":
     app.run(debug=True)
